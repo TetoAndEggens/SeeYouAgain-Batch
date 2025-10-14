0c009ba9b0b96cee5403780582208eec46f0c346
bronze_yun_4521 <ehddbs452100@naver.com>
2025-10-14 16:21:57 +0900

    feat: init project

---
+++ src/main/java/tetoandeggens/seeyouagainbatch/SeeYouAgainBatchApplication.java
@@ -0,0 +1,13 @@
+package tetoandeggens.seeyouagainbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeeYouAgainBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeeYouAgainBatchApplication.class, args);
+	}
+
+}
