945df7f7f9483ef28f4278991e6d87fe3797f850
bronze_yun_4521 <ehddbs452100@naver.com>
2025-10-30 02:22:19 +0900

    feat: #7 add feign library

--- src/main/java/tetoandeggens/seeyouagainbatch/SeeYouAgainBatchApplication.java
+++ src/main/java/tetoandeggens/seeyouagainbatch/SeeYouAgainBatchApplication.java
@@ -2,12 +2,13 @@ package tetoandeggens.seeyouagainbatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class SeeYouAgainBatchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SeeYouAgainBatchApplication.class, args);
 	}
-
 }
