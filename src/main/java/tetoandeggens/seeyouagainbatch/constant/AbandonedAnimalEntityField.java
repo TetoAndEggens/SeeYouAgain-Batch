ab6aa94db4f2559898c80f9e88ba3d717ec24a47
bronze_yun_4521 <ehddbs452100@naver.com>
2025-10-30 02:24:26 +0900

    feat: #7 set jdbc database field enum class

---
+++ src/main/java/tetoandeggens/seeyouagainbatch/constant/AbandonedAnimalEntityField.java
@@ -0,0 +1,34 @@
+package tetoandeggens.seeyouagainbatch.constant;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum AbandonedAnimalEntityField {
+
+    DESERTION_NO("desertion_no"),
+    HAPPEN_DATE("happen_date"),
+    HAPPEN_PLACE("happen_place"),
+    CITY("city"),
+    TOWN("town"),
+    SPECIES("species"),
+    COLOR("color"),
+    BIRTH("birth"),
+    WEIGHT("weight"),
+    NOTICE_NO("notice_no"),
+    NOTICE_START_DATE("notice_start_date"),
+    NOTICE_END_DATE("notice_end_date"),
+    PROCESS_STATE("process_state"),
+    SEX("sex"),
+    NEUTERED_STATE("neutered_state"),
+    SPECIAL_MARK("special_mark"),
+    CENTER_PHONE("center_phone"),
+    FINAL_UPDATED_AT("final_updated_at"),
+    CENTER_LOCATION_ID("center_location_id"),
+    BREED_TYPE_ID("breed_type_id"),
+    CREATED_AT("created_at"),
+    UPDATED_AT("updated_at");
+
+    private final String columnName;
+}
