dc58b6551dcea04f58b2d4f5c7599cf68bda75d5
bronze_yun_4521 <ehddbs452100@naver.com>
2025-10-31 22:29:01 +0900

    refactor: #7 add all constructor

--- src/main/java/tetoandeggens/seeyouagainbatch/job/abandonedanimaldataload/dto/AbandonedAnimalPublicDataDto.java
+++ src/main/java/tetoandeggens/seeyouagainbatch/job/abandonedanimaldataload/dto/AbandonedAnimalPublicDataDto.java
@@ -1,11 +1,13 @@
 package tetoandeggens.seeyouagainbatch.job.abandonedanimaldataload.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class AbandonedAnimalPublicDataDto {
 
     @JsonProperty("desertionNo")
--- src/main/java/tetoandeggens/seeyouagainbatch/job/abandonedanimaldataload/dto/KakaoMapResponseWrapper.java
+++ src/main/java/tetoandeggens/seeyouagainbatch/job/abandonedanimaldataload/dto/KakaoMapResponseWrapper.java
@@ -1,6 +1,7 @@
 package tetoandeggens.seeyouagainbatch.job.abandonedanimaldataload.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import java.util.List;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class KakaoMapResponseWrapper {
 
     @JsonProperty("documents")
