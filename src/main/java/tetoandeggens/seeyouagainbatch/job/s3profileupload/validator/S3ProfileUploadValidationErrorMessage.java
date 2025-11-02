6833ade8789706bc577bf20c7abc4dcc53889097
bronze_yun_4521 <ehddbs452100@naver.com>
2025-11-02 19:06:47 +0900

    feat: #14 validate s3 uploading job's parameters

---
+++ src/main/java/tetoandeggens/seeyouagainbatch/job/s3profileupload/validator/S3ProfileUploadValidationErrorMessage.java
@@ -0,0 +1,20 @@
+package tetoandeggens.seeyouagainbatch.job.s3profileupload.validator;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum S3ProfileUploadValidationErrorMessage {
+
+	PARAMETERS_NULL("Job 파라미터가 없습니다"),
+	PARAMETER_REQUIRED("%s 파라미터는 필수값입니다"),
+	INVALID_DATE_FORMAT("%s 날짜 형식이 올바르지 않습니다: %s (올바른 형식: yyyyMMdd)"),
+	START_DATE_AFTER_END_DATE("시작일이 종료일보다 늦을 수 없습니다. 시작일: %s, 종료일: %s");
+
+	private final String messageFormat;
+
+	public String format(Object... args) {
+		return String.format(messageFormat, args);
+	}
+}
