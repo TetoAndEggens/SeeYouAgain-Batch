3e6e1b1cb4dd55ef830c60178f6b47c741a0bd55
ehddbs4521 <ehddbs452100@naver.com>
2025-12-14 23:13:12 +0900

    refactor: #37 set multi async thread

---
+++ src/main/java/tetoandeggens/seeyouagainbatch/config/NamedThreadFactory.java
@@ -0,0 +1,21 @@
+package tetoandeggens.seeyouagainbatch.config;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class NamedThreadFactory implements ThreadFactory {
+
+	private final AtomicInteger threadNumber = new AtomicInteger(1);
+	private final String namePrefix;
+
+	public NamedThreadFactory(String namePrefix) {
+		this.namePrefix = namePrefix;
+	}
+
+	@Override
+	public Thread newThread(Runnable r) {
+		Thread thread = new Thread(r, namePrefix + "-" + threadNumber.getAndIncrement());
+		thread.setDaemon(false);
+		return thread;
+	}
+}
